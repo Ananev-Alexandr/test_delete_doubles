5b031b9010a414262fd762a3b6ad18e7ac52efc9
Ananev-Alexandr <ananevalexandr@mail.ru>
2022-12-26 19:50:31 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import json
+
+
+test_list = [{"key1": "value1"},
+             {"k1": "v1", "k2": "v2",
+             "k3": "v3"},
+             {},
+             {},
+             {"key1": "value1"},
+             {"key1": "value1"},
+             {"key2": "value2"}]
+
+def delete_doubles(lis: list):
+    my_set = set()
+    for dict_ in lis:
+        my_set.add(json.dumps(dict_, sort_keys=True))
+    return my_set
+
+print(delete_doubles(test_list))
